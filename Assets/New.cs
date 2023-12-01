2b975a36ff7c8d5b8b922c85328eb75cf5217e34
Leejungsuk96 <svwsv3462@gmail.com>
2023-12-01 15:59:47 +0900

    스크립트 추가

---
+++ Assets/New.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    int i; //선언했다.
+
+    string s = "안녕"; // string은 문자여러개  //선언했다 할당했다
+    char c = 'a'; //char 는 문자 하나
+    bool c = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 5; //할당했다
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //초기화 - 1
+    private void Awake()
     {
-        
+        Debug.Log("초기화 1"); 
+    }
+    // Start is called before the first frame update
+   //초기화 - 2
+   private void Start()
+    {
+        Debug.Log("초기화 2");
     }
 
     // Update is called once per frame
-    void Update()
+    // 매 Frame 마다 -4
+    private void Update()
     {
-        
+        //transform.Position
+        //Time.deltatime
+    }
+    // 물리연산 끝나고 호출 -3
+    private void FixedUpdate()
+    {
+        //rigidbody
+    }
+    // 위에 애들 다 끝나고 -5
+    private void LateUpdate()
+    {
+        //카메라 찍기
+    }
+    private void OnEnable()
+    {
+        Debug.Log("켜졌다.");
+        //게임 오브젝트가 켜질때
+    }
+    private void OnDisable()
+    {
+        Debug.Log("꺼졌다.");
+        //게임 오브젝트가 꺼질때
     }
 }
